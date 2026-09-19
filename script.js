a75a08b3d90204d9a3af17e759e559e1290f6df4
stomeli <ormundoste@gmail.com>
2026-09-19 07:14:51 -0300

    controle de sistema

--- script.js
+++ script.js
@@ -2,13 +2,14 @@
 SUPABASE
 ========================================================= */
 
-const SUPABASE_URL = "https://dfqclkfqdzfsftnmzejx.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_IV8xYF0qDVVYAxhSgSjqtg_mreIReGf";
+const SUPABASE_URL = "URL_DO_NOVO_PROJETO";
+const SUPABASE_ANON_KEY = "CHAVE_PUBLICA_DO_NOVO_PROJETO";
 
 const db = window.supabase.createClient(
-SUPABASE_URL,
-SUPABASE_ANON_KEY
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
 );
+
 
 /* =========================================================
 DADOS
